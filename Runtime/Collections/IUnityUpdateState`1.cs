ee872ea5ed49d2f11270db02f5fa9b334045b89f
谢凯文 <844038603@qq.com>
2025-04-24 00:02:25 +0800

    no message

--- Runtime/Collections/IUnityUpdateState`1.cs
+++ Runtime/Collections/IUnityUpdateState`1.cs
@@ -1,4 +1,3 @@
-using System;
 using Aurora.Collections;
 
 namespace Aurora.Unity.Collections
@@ -14,8 +13,6 @@ namespace Aurora.Unity.Collections
         /// 有限状态机在 Unity 帧更新时执行自定义逻辑。
         /// </summary>
         /// <param name="stateMachine">有限状态机。</param>
-        /// <exception cref="NotSupportedException">这个状态不是可持续状态。换言之，它总是在 <see cref="IState{T}.OnEnter"/> 中调用 <see cref="StateMachine{T}.TransitionToDuringNextUpdate"/> 或 <see cref="StateMachine{T}.TransitionToNullDuringNextUpdate"/>。</exception>
-        /// <remarks>如果这个状态不是可持续状态，请显式地抛出 <see cref="NotSupportedException"/>。</remarks>
         void OnUnityUpdate(UnityUpdateStateMachine<T> stateMachine);
     }
 }
--- Runtime/Collections/UnityUpdateStateMachine`1.cs
+++ Runtime/Collections/UnityUpdateStateMachine`1.cs
@@ -7,28 +7,13 @@ namespace Aurora.Unity.Collections
     /// <summary>
     /// 在 Unity 帧更新时执行自定义操作的有限状态机。
     /// </summary>
-    /// <remarks>只接受 <see cref="IUnityUpdateState{T}"/> 类型的状态。</remarks>
     public class UnityUpdateStateMachine<T> : StateMachine<T>
     {
-        /// <summary>
-        /// 如果 <see cref="state"/> 不是 <see cref="IUnityUpdateState{T}"/>，则抛出 <see cref="ArgumentException"/>。
-        /// </summary>
-        /// <param name="state">状态。</param>
-        /// <exception cref="ArgumentException"><see cref="state"/> 不是 <see cref="IUnityUpdateState{T}"/>。</exception>
-        protected override void ThrowIfRejectState(IState<T> state)
-        {
-            if (state is IUnityUpdateState<T>)
-            {
-                return;
-            }
-            throw new ArgumentException(null, nameof(state));
-        }
-
         /// <summary>
         /// 在 Unity 帧更新时执行自定义逻辑。
         /// </summary>
         /// <exception cref="InvalidOperationException">有限状态机正在进入或退出状态。</exception>
-        /// <remarks>在帧更新时调用一次。</remarks>
+        /// <remarks>在每帧的某个时刻，先反复调用 <see cref="StateMachine{T}.Update"/> 直到返回值为 <see langword="false"/>，然后调用此方法一次。</remarks>
         public void UnityUpdate()
         {
             ThrowIfEnteringOrExiting();
