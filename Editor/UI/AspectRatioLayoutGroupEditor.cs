7d33639b579777000e3ef57b39fbcfb65f77030d
谢凯文 <844038603@qq.com>
2025-05-03 15:55:24 +0800

    宽高比布局组
    - 实现 OneElementLayoutGroup 基类，为单元素布局组提供支持
    - 新增 AspectRatioLayoutGroup 类，用于保持指定的宽高比

---
+++ Editor/UI/AspectRatioLayoutGroupEditor.cs
@@ -0,0 +1,25 @@
+﻿using Aurora.Unity.UI;
+using UnityEditor;
+
+namespace Aurora.UnityEditor.UI
+{
+    [CustomEditor(typeof(AspectRatioLayoutGroup))]
+    [CanEditMultipleObjects]
+    internal sealed class AspectRatioLayoutGroupEditor : OneElementLayoutGroupEditor
+    {
+        private SerializedProperty _aspectRatio;
+
+        private void OnEnable()
+        {
+            _aspectRatio = serializedObject.FindProperty(nameof(AspectRatioLayoutGroup.aspectRatio));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(_aspectRatio);
+            serializedObject.ApplyModifiedProperties();
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ Editor/UI/OneElementLayoutGroupEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Aurora.Unity.UI;
+using UnityEditor;
+
+namespace Aurora.UnityEditor.UI
+{
+    /// <summary>
+    /// 为 <see cref="OneElementLayoutGroup"/> 类的派生类的自定义编辑器提供基类。
+    /// </summary>
+    public abstract class OneElementLayoutGroupEditor : Editor
+    {
+        /// <inheritdoc />
+        public override void OnInspectorGUI()
+        {
+            if (targets.Cast<OneElementLayoutGroup>().All(ContainsExactlyOneElement))
+            {
+                return;
+            }
+            EditorGUILayout.HelpBox(
+                "This layout group won't work properly because it doesn't contain exactly one layout element.",
+                MessageType.Warning
+            );
+
+            static bool ContainsExactlyOneElement(OneElementLayoutGroup oneElementLayoutGroup)
+            {
+                return oneElementLayoutGroup.ContainsExactlyOneElement();
+            }
+        }
+    }
+}
