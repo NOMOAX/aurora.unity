cec8b86143f08e499fd314c909cc392fc4cca46b
谢凯文 <844038603@qq.com>
2025-08-01 17:33:42 +0800

    （上一条提交中遗忘的条目）

---
+++ Runtime/When.cs
@@ -0,0 +1,23 @@
+﻿namespace Aurora.Unity
+{
+    /// <summary>
+    /// 何时。
+    /// </summary>
+    public enum When : byte
+    {
+        /// <summary>
+        /// 总是。
+        /// </summary>
+        Always,
+
+        /// <summary>
+        /// 运行中。
+        /// </summary>
+        Playing,
+
+        /// <summary>
+        /// 非运行时。
+        /// </summary>
+        NotPlaying
+    }
+}
