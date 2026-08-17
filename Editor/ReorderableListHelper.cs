d7b27f8634ad4e58386e48e41ef665d34bab5a96
谢凯文 <844038603@qq.com>
2026-08-17 22:06:59 +0800

    no message

--- Editor/ReorderableListHelper.cs
+++ Editor/ReorderableListHelper.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 namespace Aurora.UnityEditor
 {
     /// <summary>
-    /// 提供在使用 <see cref="ReorderableList"/> 时需要用到的辅助方法。
+    /// 提供在使用 <see cref="ReorderableList"/> 时需要用到的常量和辅助方法。
     /// </summary>
     public static class ReorderableListHelper
     {
@@ -19,7 +19,7 @@ namespace Aurora.UnityEditor
         /// 这个值既是相邻元素的间距（由 <see cref="ReorderableList"/> 添加，不可控制），又是元素内部相邻行的间距（我规定的，不服来干我）。
         /// </summary>
         /// <remarks>原始值为 <see cref="ReorderableList.Defaults"/><c>.elementPadding</c>。</remarks>
-        private const float VerticalSpacing = 2f;
+        public const float VerticalSpacing = 2f;
 
         /// <summary>
         /// 获取具有指定行数的元素的高度，用于作为 <see cref="ReorderableList.elementHeightCallback"/> 回调方法的返回值。
